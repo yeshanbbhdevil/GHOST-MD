08a77cb93f2bd321d34a677128aeb04608b4d311
yeshanbbhdevil <yeshanbasnayake20071001@gmail.com>
2024-11-08 06:24:27 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,8 +3,8 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "GHOST-MD&JTqi7uct",
-  PREFIX: process.env.PREFIX || '.',
+  SESSION_ID: process.env.SESSION_ID || "GHOST-MD&xY5zHiXA",
+  PREFIX: process.env.PREFIX || '/',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -20,7 +20,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "𝗚𝗛𝗢𝗦𝗧-𝗠𝗗",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94741140620",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "9401219309",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
